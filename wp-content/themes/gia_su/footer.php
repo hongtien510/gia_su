690ed1159cf03b49e9f65ef2fc7bd199cf653958
[TienPham] <[tien@bliss-interactive.net]>
2015-07-27 22:59:53 +0700

    .

--- wp-content/themes/gia_su/footer.php
+++ wp-content/themes/gia_su/footer.php
@@ -6,19 +6,23 @@
 <div class="clear"></div>
 <div class="footer-bottom">
 	<div class="container">
-		<div class="alignright">
+		<div class="alignright footer-right">
 			<?php
 				$footer_vars = array('%year%' , '%site%' , '%url%');
 				$footer_val  = array( date('Y') , get_bloginfo('name') , home_url() );
 				$footer_two  = str_replace( $footer_vars , $footer_val , tie_get_option( 'footer_two' ));
 				echo htmlspecialchars_decode( $footer_two );?>
+			
 		</div>
-		<?php if( tie_get_option('footer_social') ) tie_get_social( true , false, 'ttip-none' ); ?>
 		
 		<div class="alignleft">
+			<?php if( tie_get_option('footer_social') ) tie_get_social( true , false, 'ttip-none' ); ?>
+			<div class="copyright">
 			<?php
 				$footer_one  = str_replace( $footer_vars , $footer_val , tie_get_option( 'footer_one' ));
-				echo htmlspecialchars_decode( $footer_one );?>
+				echo htmlspecialchars_decode( $footer_one );
+			?>
+			</div>
 		</div>
 		<div class="clear"></div>
 	</div><!-- .Container -->
